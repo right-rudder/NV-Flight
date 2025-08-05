7a11904f98e582304e66dd0958ccbb6194ef80cb
karlavdf <karlavdelgadof@gmail.com>
2025-08-05 15:20:40 -0500

    CTA colors

--- src/data/fly-with-us-cta.js
+++ src/data/fly-with-us-cta.js
@@ -1,9 +1,9 @@
 const flyWithUsCTA = {
   imagePath: "/src/assets/discovery-flight-nv.webp",
   imageAlt: "NV Flight Flight School Discovery Flight",
-  headerH1: `<span class="text-accent-600">Fly with the team that makes</br></span>becoming a pilot simple`,
-  paragraph:
-    "We are a team of aviation experts in Reno, NV with decades of combined experience in the flight training industry. Our simple and effective training programs are designed to get you safely in the air as quickly as possible.",
+  paragraph1: "Fly With Us",
+  headerH1: `Discover the joy of reaching new heights`,
+  paragraph: `We are a team of aviation experts in Reno, NV with decades of combined experience in the flight training industry. Our simple and effective training programs are designed to get you safely in the air as quickly as possible.`,
   buttons: [
     {
       name: "START FLYING TODAY",
