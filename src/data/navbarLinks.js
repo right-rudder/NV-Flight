763802273de8dbe037a66c8976926bfa54f9d084
Diego Yon <diegoyon@outlook.com>
2025-08-20 14:51:08 -0500

    fix styles

--- src/data/navbarLinks.js
+++ src/data/navbarLinks.js
@@ -1,63 +1,63 @@
 export const navbarLinks = {
   center: [
-  {
-    name: "Become a Pilot",
-    link: "/#",
-  },
-  {
-    name: "Programs",
-    link: "/programs",
-    submenu: [
-      {
-        name: "Private Pilot ASEL",
-        link: "/programs/private-pilot-asel",
-      },
-      {
-        name: "Instrument Pilot ASEL",
-        link: "/programs/instrument-pilot-asel",
-      },
-      {
-        name: "Commercial Pilot ASEL",
-        link: "/programs/commercial-pilot-asel",
-      },
-      {
-        name: "Flight Instructor ASEL",
-        link: "/programs/flight-instructor-asel",
-      },
-      {
-        name: "Flight Instructor Instrument",
-        link: "/programs/flight-instructor-instrument",
-      },
-      {
-        name: "Mountain Flight Training",
-        link: "/programs/mountain-flight-training",
-      }
-    ],
-  },
-  {
-    name: "About Us",
-    link: "/nv-flight-about",
-    submenu: [
-      {
-        name: "Our Team",
-        link: "/nv-flight-about/team",
-      },
-      {
-        name: "Our Fleet",
-        link: "/nv-flight-about/our-fleet",
-      },
-      {
-        name: "Blog",
-        link: "/nv-flight-about/blog",
-      },
-    ],
-  },
+    {
+      name: "Become a Pilot",
+      link: "/become-a-pilot",
+    },
+    {
+      name: "Programs",
+      link: "/programs",
+      submenu: [
+        {
+          name: "Private Pilot ASEL",
+          link: "/programs/private-pilot-asel",
+        },
+        {
+          name: "Instrument Pilot ASEL",
+          link: "/programs/instrument-pilot-asel",
+        },
+        {
+          name: "Commercial Pilot ASEL",
+          link: "/programs/commercial-pilot-asel",
+        },
+        {
+          name: "Flight Instructor ASEL",
+          link: "/programs/flight-instructor-asel",
+        },
+        {
+          name: "Flight Instructor Instrument",
+          link: "/programs/flight-instructor-instrument",
+        },
+        {
+          name: "Mountain Flight Training",
+          link: "/programs/mountain-flight-training",
+        },
+      ],
+    },
+    {
+      name: "About Us",
+      link: "/nv-flight-about",
+      submenu: [
+        {
+          name: "Our Team",
+          link: "/nv-flight-about/team",
+        },
+        {
+          name: "Our Fleet",
+          link: "/nv-flight-about/our-fleet",
+        },
+        {
+          name: "Blog",
+          link: "/nv-flight-about/blog",
+        },
+      ],
+    },
   ],
 
   buttons: [
     {
       name: "Enroll Now",
-      link: "/#",
+      link: "/enroll",
       submenu: [],
       primary: true,
     },
@@ -67,5 +67,5 @@ export const navbarLinks = {
       submenu: [],
       primary: false,
     },
-  ]
+  ],
 };
