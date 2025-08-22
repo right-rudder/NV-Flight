924555d455a53ece7996caa45758ec9a1c095d8a
karlavdf <karlavdelgadof@gmail.com>
2025-08-21 23:24:35 -0500

    Add podcast component

---
+++ src/data/podcastData.js
@@ -0,0 +1,25 @@
+// src/data/podcastData.js
+const podcastData = {
+  title: "Get yourself Ready for Takeoff",
+  description: "Actionable insights and real stories in every episode.",
+  href: "https://open.spotify.com/show/7c4tRGyqoPL2fWXOJuTuDJ?si=f821309a129c4d80",
+  embedSrc:
+    "https://open.spotify.com/embed/show/7c4tRGyqoPL2fWXOJuTuDJ?utm_source=generator",
+
+  appleEmbedSrc:
+    "https://embed.podcasts.apple.com/us/podcast/nv-flight-briefing-always-answer-c/id1795141117?i=1000690137727",
+  ctaButtons: [
+    {
+      label: "Listen on Spotify",
+      href: "https://open.spotify.com/show/7c4tRGyqoPL2fWXOJuTuDJ?si=f821309a129c4d80",
+    },
+    {
+      label: "Listen on Apple Podcasts",
+      href: "https://podcasts.apple.com/us/podcast/nv-flight-briefing/id1795141117",
+    },
+  ],
+
+  eyebrow: "Our Podcast",
+};
+
+export default podcastData;
