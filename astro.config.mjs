94e49768b579f945936e7c8965886167dbf7b1b7
karlavdf <karlavdelgadof@gmail.com>
2025-08-25 12:29:38 -0500

    Robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://NVFlightco.com/",
+  site: "https://nv-flight.com",
   integrations: [tailwind(), sitemap(), react()],
   redirects: {
     "/posts/[...slug]": "/blog/[...slug]",
