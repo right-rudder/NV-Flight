da01162bdef31d0f68b5a90d31e34d2c065f186d
lucascr <lcrodrigues1995@gmail.com>
2025-08-27 15:32:59 -0300

    Update coursesIndex.js

--- src/data/coursesIndex.js
+++ src/data/coursesIndex.js
@@ -5,7 +5,7 @@ const coursesIndex = {
     // SEO Header Stuff
     pageTitle: `Training Programs | ${COMPANY_NAME}`,
     pageDescription:
-      "Discover NVFlight Flight School’s comprehensive pilot training programs. From Private Pilot License to advanced certifications like Instrument Rating, Multi-Engine, and Certified Flight Instructor, our expert-led courses are designed to help you achieve your aviation dreams.",
+      "Discover NV Flight Flight School’s comprehensive pilot training programs. From Private Pilot License to advanced certifications like Instrument Rating, Multi-Engine, and Certified Flight Instructor, our expert-led courses are designed to help you achieve your aviation dreams.",
     pageKeywords:
       "flight training courses Arizona, aviation certifications, private to commercial pilot, multi-engine rating training, CFI courses Reno, aviation school programs",
 
@@ -14,13 +14,13 @@ const coursesIndex = {
       stars: true,
       imagePath: "/src/assets/montain-&-plane2.jpg",
       imageAlt:
-        "Photograph of a NVFlight Flight School aircraft soaring over the Arizona landscape",
+        "Photograph of a NV Flight Flight School aircraft soaring over the Arizona landscape",
       headerH1: `Pilot <br>PROGRAMS</span>`,
-      paragraph: `Join NVFlight Flight School and take your first step towards a successful aviation career. Whether you're starting with a discovery flight or pursuing advanced ratings, we’re here to support you every step of the way.`,
+      paragraph: `Join NV Flight Flight School and take your first step towards a successful aviation career. Whether you're starting with a discovery flight or pursuing advanced ratings, we’re here to support you every step of the way.`,
       buttons: [
         {
           name: "Enroll Today",
-          link: "/enroll-at-NVFlight",
+          link: "/enroll",
           primary: false,
         },
         {
