dbc8d5b07aeb4c1d46a74dd03cbfabb86973c65f
lucascr <lcrodrigues1995@gmail.com>
2025-08-27 15:33:03 -0300

    Update CTAFlight.js

--- src/data/CTAFlight.js
+++ src/data/CTAFlight.js
@@ -1,9 +1,9 @@
 const CTAFlight = {
   imagePath: "/src/assets/NVFlight-cta-bg.webp",
-  imageAlt: "NVFlight Flight School student flying over Arizona",
+  imageAlt: "NV Flight Flight School student flying over Arizona",
   headerH1: `<span class="text-primary-800">Take the First Step</br></span>With a Discovery Flight`,
   paragraph:
-    "Experience the thrill of flying with a discovery flight at NVFlight Flight School. Whether you're exploring aviation for the first time or considering a professional pilot career, this is your opportunity to take control and soar over Arizona. Book your discovery flight today and begin your journey into aviation!",
+    "Experience the thrill of flying with a discovery flight at NV Flight Flight School. Whether you're exploring aviation for the first time or considering a professional pilot career, this is your opportunity to take control and soar over Arizona. Book your discovery flight today and begin your journey into aviation!",
   buttons: [
     {
       name: "Book Now",
