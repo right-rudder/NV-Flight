e8fbab999f583da674625299bf25e21b1de25933
lucascr <lcrodrigues1995@gmail.com>
2025-08-27 15:33:18 -0300

    Update fly-with-us-cta.js

--- src/data/fly-with-us-cta.js
+++ src/data/fly-with-us-cta.js
@@ -1,6 +1,6 @@
 const flyWithUsCTA = {
   imagePath: "/src/assets/montain-&-plane2.jpg",
-  imageAlt: "NV Flight Flight School Discovery Flight",
+  imageAlt: "NV Flight School Discovery Flight",
   paragraph1: "Fly With Us",
   headerH1: `Discover the joy of reaching new heights`,
   paragraph: `We are a team of aviation experts in Reno, NV with decades of combined experience in the flight training industry. Our simple and effective training programs are designed to get you safely in the air as quickly as possible.`,
