f47a24c9355d4a93e5994e8fa1c77b51d77be210
lucascr <lcrodrigues1995@gmail.com>
2025-08-27 15:34:00 -0300

    Update pilot-training-made-simple.js

--- src/data/pilot-training-made-simple.js
+++ src/data/pilot-training-made-simple.js
@@ -1,9 +1,9 @@
 const pilotTrainingMadeSimple = {
   data: {
     //SEO Header Stuff
-    pageTitle: "Pilot Training Made Simple | NVFlight Flight School Reno, NV",
+    pageTitle: "Pilot Training Made Simple | NV Flight Flight School Reno, NV",
     pageDescription:
-      "NVFlight Flight School  offers flight training in Reno, NV. We offer private pilot training, instrument rating training, commercial pilot training, and more. Contact us today!",
+      "NV Flight Flight School  offers flight training in Reno, NV. We offer private pilot training, instrument rating training, commercial pilot training, and more. Contact us today!",
     pageKeywords:
       "flight training, flight school, pilot training, private pilot training, instrument rating training, commercial pilot training, Reno, NV",
 
@@ -11,16 +11,16 @@ const pilotTrainingMadeSimple = {
     header: {
       stars: true,
       imagePath:
-        "/src/assets/NVFlight-flight-school-airplane-landing-at-falcon-field-airport-pilot-training-Reno-arizona.webp",
+        "/src/assets/NV Flight-flight-school-airplane-landing-at-falcon-field-airport-pilot-training-Reno-arizona.webp",
       imageAlt:
-        "NVFlight Flight School airplane landing at Falcon Field Airport in Reno, Arizona",
+        "NV Flight Flight School airplane landing at Falcon Field Airport in Reno, Arizona",
       headerH1: `PILOT TRAINING</br><span class="text-primary-300">MADE SIMPLE</span>`,
       paragraph:
-        "How would you like to learn to fly? At NVFlight, we make it easy to get started. Our flight school offers a variety of pilot training programs to help you achieve your aviation goals.",
+        "How would you like to learn to fly? At NV Flight, we make it easy to get started. Our flight school offers a variety of pilot training programs to help you achieve your aviation goals.",
       buttons: [
         {
           name: "Enroll Now",
-          link: "/enroll-at-NVFlight",
+          link: "/enroll",
           primary: false,
         },
       ],
@@ -29,7 +29,7 @@ const pilotTrainingMadeSimple = {
     pageSummary: {
       title: "Where Do I Start?",
       paragraphs: [
-        "Learning to fly can be a daunting task for beginners. It’s like learning a whole new language, and the path to success might not always be clear. But don’t worry, we’ve got you covered at NVFlight Flight School!",
+        "Learning to fly can be a daunting task for beginners. It’s like learning a whole new language, and the path to success might not always be clear. But don’t worry, we’ve got you covered at NV Flight Flight School!",
         "We’ve designed our training program to be efficient and easy to understand. Keep reading to learn more about our flight training schedules, how we progress students, and what we expect from our career track students.",
       ],
       buttons: [
