75f7dbb082fac941e548e98e70713d5966d82175
karlavdf <karlavdelgadof@gmail.com>
2025-08-28 16:21:42 -0500

    Fix hero images

--- src/data/contact-info.js
+++ src/data/contact-info.js
@@ -10,7 +10,7 @@ const contactInfo = {
     checkboxText: `I agree to <a href="/terms-of-service" target="_blank" class="font-semibold hover:underline">terms</a> & <a href="/privacy-policy" target="_blank" class="font-semibold hover:underline">conditions</a> provided by the company. By providing my phone number, I agree to receive text messages from the business.`,
 
     header: {
-      imagePath: "/src/assets/nvflight-school-reno-aircraft.jpg",
+      imagePath: "/src/assets/nv-flight-reno_20220505_084802_205_Original.webp",
       imageAlt: "NV Flight School aircraft flying over Reno, NV",
       headerH1: "Contact NV Flight School",
       paragraph:
--- src/data/enroll-info.js
+++ src/data/enroll-info.js
@@ -10,7 +10,7 @@ const enrollInfo = {
     checkboxText: `I agree to <a href="/terms-of-service" target="_blank" class="font-semibold hover:underline">terms</a> & <a href="/privacy-policy" target="_blank" class="font-semibold hover:underline">conditions</a> provided by the company. By providing my phone number, I agree to receive text messages from the business.`,
 
     header: {
-      imagePath: "/src/assets/nvflight-school-reno-aircraft.jpg",
+      imagePath: "/src/assets/nvflight-reno-enroll-hero.webp",
       imageAlt: "NV Flight School aircraft flying over Reno, NV",
       headerH1: "Enroll Now",
       paragraph:
