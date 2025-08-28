9123a3a05dacccf8b741dfafa0997778420e73df
Diego Yon <diegoyon@outlook.com>
2025-08-28 15:26:54 -0500

    add review

--- src/data/navbarLinks.js
+++ src/data/navbarLinks.js
@@ -1,65 +1,65 @@
 export const navbarLinks = {
   center: [
-  {
-    name: "Become a Pilot",
-    link: "/become-a-pilot",
-  },
-  {
-    name: "Programs",
-    link: "/programs",
-    submenu: [
-      {
-        name: "Programs Index",
-        link: "/programs",
-      },
-      {
-        name: "Private Pilot ASEL",
-        link: "/programs/private-pilot-asel",
-      },
-      {
-        name: "Instrument Rating ASEL",
-        link: "/programs/instrument-rating-asel",
-      },
-      {
-        name: "Commercial Pilot ASEL",
-        link: "/programs/commercial-pilot-asel",
-      },
-      {
-        name: "Flight Instructor ASEL",
-        link: "/programs/flight-instructor-asel",
-      },
-      {
-        name: "Flight Instructor Instrument",
-        link: "/programs/flight-instructor-instrument",
-      },
-      {
-        name: "Mountain Flight Training",
-        link: "/programs/mountain-flight-training",
-      }
-    ],
-  },
-  {
-    name: "About Us",
-    link: "/nv-flight-about",
-    submenu: [
-      {
-        name: "About Us",
-        link: "/nv-flight-about",
-      },
-      {
-        name: "Our Team",
-        link: "/nv-flight-about/team",
-      },
-      {
-        name: "Our Fleet",
-        link: "/nv-flight-about/our-fleet",
-      },
-      {
-        name: "Blog",
-        link: "/blog",
-      },
-    ],
-  },
+    {
+      name: "Become a Pilot",
+      link: "/become-a-pilot",
+    },
+    {
+      name: "Programs",
+      link: "/programs",
+      submenu: [
+        {
+          name: "All Programs",
+          link: "/programs",
+        },
+        {
+          name: "Private Pilot ASEL",
+          link: "/programs/private-pilot-asel",
+        },
+        {
+          name: "Instrument Rating ASEL",
+          link: "/programs/instrument-rating-asel",
+        },
+        {
+          name: "Commercial Pilot ASEL",
+          link: "/programs/commercial-pilot-asel",
+        },
+        {
+          name: "Flight Instructor ASEL",
+          link: "/programs/flight-instructor-asel",
+        },
+        {
+          name: "Flight Instructor Instrument",
+          link: "/programs/flight-instructor-instrument",
+        },
+        {
+          name: "Mountain Flight Training",
+          link: "/programs/mountain-flight-training",
+        },
+      ],
+    },
+    {
+      name: "About Us",
+      link: "/nv-flight-about",
+      submenu: [
+        {
+          name: "About Us",
+          link: "/nv-flight-about",
+        },
+        {
+          name: "Our Team",
+          link: "/nv-flight-about/team",
+        },
+        {
+          name: "Our Fleet",
+          link: "/nv-flight-about/our-fleet",
+        },
+        {
+          name: "Blog",
+          link: "/blog",
+        },
+      ],
+    },
   ],
 
   buttons: [
